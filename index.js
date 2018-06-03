995c6c39203a502c3f9178f4fc43efc07e5b076c
Peter MOUEZA <mouezapeter@gmail.com>
2018-06-03 21:42:53 +0200

    no19

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ console.log("no18");
   
   
   
-  var betax   = document.querySelector('.betax');
-var gammay = document.querySelector('.gammay');
-var alphaz = document.querySelector('.alphaz');
+  /**https://www.w3schools.com/jsref/met_document_queryselector.asp*/
+  var betax   = document.querySelector('.betasx');
+  
+var gammay = document.querySelector('.gammasy');
+var alphaz = document.querySelector('.alphasz');
 
   //https://developer.mozilla.org/en-US/docs/Web/API/Element/clientWidth
-var maxX = garden.clientWidth  - ball.clientWidth;
-var maxY = garden.clientHeight - ball.clientHeight;
+//var maxX = garden.clientWidth  - ball.clientWidth;
+//var maxY = garden.clientHeight - ball.clientHeight;
 
 function handleOrientation(event) {
   var x = event.beta;  // In degree in the range [-180,180]
@@ -21,16 +23,16 @@ function handleOrientation(event) {
   var z = event.alpha;  // 
 
   betax.innerHTML  = "beta : " + x + "\n";
-  gammay.innerHTML += "gamma: " + y + "\n";
-alphaz.innerHTML += "alpha: " + z + "\n";
+  gammay.innerHTML = "gamma: " + y + "\n";
+alphaz.innerHTML = "alpha: " + z + "\n";
   
-
+/*
 document.getElementById("betax").innerHTML = x;
   
   document.getElementById("gammay").innerHTML = y;
   
   document.getElementById("alphaz").innerHTML = z;
-  
+  */
   
   
   // Because we don't want to have the device upside down
@@ -40,13 +42,13 @@ document.getElementById("betax").innerHTML = x;
 
   // To make computation easier we shift the range of 
   // x and y to [0,180]
-  x += 90;
-  y += 90;
+  //x += 90;
+  //y += 90;
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
-  ball.style.top  = (maxX*x/180 - 10) + "px";
-  ball.style.left = (maxY*y/180 - 10) + "px";
+ // ball.style.top  = (maxX*x/180 - 10) + "px";
+ // ball.style.left = (maxY*y/180 - 10) + "px";
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
